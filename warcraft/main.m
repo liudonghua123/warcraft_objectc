0492dd4bc38544992b61d3609e22ff9b1b765e42
liudonghua123 <liudonghua123@gmail.com>
2014-03-22 20:57:56 +0800

    Initial Commit

---
+++ warcraft/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  warcraft
+//
+//  Created by 刘东华 on 3/22/14.
+//  Copyright (c) 2014 刘东华. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
